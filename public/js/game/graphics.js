fa2792484f5051a6ccd49e06274f05c19152e1f1
dragonman117 <tj.ferrell@gmail.com>
2017-11-17 13:15:30 -0700

    Adding Interface, began graphics

---
+++ public/js/game/graphics.js
@@ -0,0 +1,32 @@
+/**
+ * Graphics.js
+ *  A simple access point to our graphics
+ **/
+
+const PIXI = require('pixi.js');
+const TiledMap = require('TiledMap');
+
+let Graphics = new function () {
+
+    const app = new PIXI.Application();
+    let renderer = null;
+
+    let init = function(initStage){
+        document.body.appendChild(app.view);
+        app.view.style.position="absolute";
+        app.view.style.display = "block";
+        app.renderer.autoResize = true;
+        app.renderer.resize(window.innerWidth, window.innerHeight);
+        app.stage = initStage;
+        app.start();
+    };
+
+    let renderStage = function(incomingStage){
+        app.renderer.render(incomingStage);
+    };
+
+    return {
+        "init": init,
+        "renderStage":renderStage
+    }
+};
---
+++ public/js/game/stage.js
@@ -0,0 +1,14 @@
+/**
+ * Stage object
+ * Generates stages for the graphics class
+ **/
+
+const PIXI = require('pixi.js');
+
+let Stage = function () {
+    let paused = false;
+    let updateController = NaN;
+
+    let base = PIXI.Stage;
+
+};
