0fee094f869f801ca370c6248cbb6fbd25334393
dragonman117 <tj.ferrell@gmail.com>
2017-11-17 22:12:59 -0700

    Moved the toload generation code to run on npm start... also updated the load to be dynamic instead of hard coding the into the loading function.

--- public/js/game/graphics.js
+++ public/js/game/graphics.js
@@ -5,15 +5,12 @@
 
 import {TiledMap} from "/js/lib/TiledMap.js";
 
+
 let Graphics = new function () {
 
     const app = new PIXI.Application();
     let renderer = null;
     PIXI.loader.use(TiledMap.middleware);
-
-    //For TEsting
-    PIXI.loader.add("128tileSet","/img/128tileSet.json");
-    PIXI.loader.add("TestMap", "/js/maps/first.map.json");
 
     let init = function(initStage){
         app.view.style.position="absolute";
@@ -23,9 +20,6 @@ let Graphics = new function () {
             (48 * 128), (27*128),
             {antialias: false, transparent: false, resolution: .5}
         );
-        // app.renderer.view.style.height = "90vh";
-        // app.renderer.view.style.position = "absolute";
-        // app.renderer.view.style.display = "block";
         app.stage = initStage;
         document.body.appendChild(app.renderer.view);
         app.start();
